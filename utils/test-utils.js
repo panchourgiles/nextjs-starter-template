dc8203b810464326bb2d73f076302cddac278c2d
Pancho Urgiles <panchourgiles@gmail.com>
2021-05-02 13:06:18 -0500

    Add new test utility to use jest with styled-components or other provider that needs wrapper the main component

---
+++ utils/test-utils.js
@@ -0,0 +1,17 @@
+import theme from '@styles/theme';
+import { render } from '@testing-library/react';
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+
+const AllTheProviders = ({ children }) => {
+  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+};
+
+const customRender = (ui, options) =>
+  render(ui, { wrapper: AllTheProviders, ...options });
+
+// re-export everything
+export * from '@testing-library/react';
+
+// override render method
+export { customRender as render };
